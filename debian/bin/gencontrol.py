3da706e69bf57b923ba235935d74b217f7e6838c
Bastian Blank <waldi@debian.org>
2005-08-05 16:38:58 +0000

    * Rewrite gencontrol. * Use debian/arch/defines, debian/arch/$arch/defines,   debian/arch/$arch/$subarch/defines and   debian/arch/$arch/$subarch/defines.$flavour to gather values. * Remove fdutils from global Suggests.
    r3646: waldi | 2005-07-31 18:48:39 +0000
    * debian/bin/gencontrol.py: Add.
    * debian/bin/gencontrol: Remove.
    * debian/rules
      - Use new gencontrol.
      - Remove makevars.
    * debian/templates/control.headers.in
    * debian/templates/control.headers.subarch.in: Add.
    * debian/templates/control.image.in: Update.
    * debian/templates/control.main.in: Remove kernel-tree entry.
    * debian/templates/control.tree.in: Move kernel-tree entry from control.main.in.
    * debian/arch/defines, debian/arch/s390/defines, debian/arch/s390/defines.s390,
      debian/arch/s390/defines.s390x: Add.
    * debian/arch/s390/desc.s390,debian/arch/s390/desc.s390x: Remove.
    
    r3647: waldi | 2005-07-31 18:57:21 +0000
    * debian/bin/gencontrol.py: Support desc keyword.
    
    r3648: waldi | 2005-07-31 19:06:30 +0000
    * debian/arch/alpha/defines, debian/arch/amd64/defines,
      debian/arch/i386/defines, debian/arch/i386/defines.686,
      debian/arch/i386/defines.686-smp, debian/arch/i386/defines.k7,
      debian/arch/i386/defines.k7-smp, debian/arch/ia64/defines,
      debian/arch/m68k/defines, debian/arch/powerpc/defines,
      debian/arch/sparc/defines, debian/arch/sparc/defines.sparc64,
      debian/arch/sparc/defines.sparc64-smp: Add.
    * debian/arch/i386/desc.686, debian/arch/i386/desc.686-smp,
      debian/arch/i386/desc.k7, debian/arch/i386/desc.k7-smp,
      debian/arch/sparc/desc.sparc64, debian/arch/sparc/desc.sparc64-smp:
      Remove
    
    r3649: waldi | 2005-07-31 19:12:49 +0000
    * debian/templates/control.image.in: Remove fdutils from Suggests.
    * debian/arch/arm/defines: Add.
    * debian/arch/alpha/defines, debian/arch/amd64/defines,
      debian/arch/i386/defines, debian/arch/ia64/defines,
      debian/arch/m68k/defines, debian/arch/powerpc/defines,
      debian/arch/sparc/defines: Update.
    
    r3650: waldi | 2005-07-31 19:44:25 +0000
    * debian/bin/gencontrol.py, debian/templates/control.headers.in:
      Update.
    
    r3680: waldi | 2005-08-03 18:12:37 +0000
    * debian/bin/gencontrol.py: Update.
    * debian/arch/i386/defines, debian/arch/s390/defines,
      debian/arch/sparc/defines: Move settings.
    * debian/arch/i386/defines.686, debian/arch/i386/defines.686-smp, 
      debian/arch/i386/defines.k7, debian/arch/i386/defines.k7-smp,
      debian/arch/s390/defines.s390, debian/arch/s390/defines.s390x,
      debian/arch/sparc/defines.sparc64, adebian/rch/sparc/defines.sparc64-smp:
      Delete.
    
    r3682: waldi | 2005-08-03 22:38:45 +0000
    * debian/arch/ia64/defines: Add class definition.s
    * debian/arch/ia64/desc.itanium-smp, debian/arch/ia64/desc.itanium
      debian/arch/ia64/desc.mckinley-smp, debian/arch/ia64/desc.mckinley:
      Delete.
    
    r3687: waldi | 2005-08-05 09:11:10 +0000
    * debian/Makefile: Copy control file.
    
    r3689: waldi | 2005-08-05 15:36:24 +0000
    * debian/rules: Make debian/changelog a prequisite of debian/control.
    
    svn path=/trunk/kernel/source/linux-2.6/; revision=3691

---
+++ debian/bin/gencontrol.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+import os, os.path, re, sys, textwrap, ConfigParser
+
+class entry(dict):
+    __slots__ = ('_list')
+
+    def __init__(self):
+        super(entry, self).__init__()
+        self._list = []
+
+    def __delitem__(self, key):
+        super(entry, self).__delitem__(key)
+        self._list.remove(key)
+
+    def __setitem__(self, key, value):
+        super(entry, self).__setitem__(key, value)
+        if key.startswith('_'):
+            return
+        if key not in self._list:
+            if 'Description' in self._list:
+                self._list.insert(len(self._list)-1, key)
+            else:
+                self._list.append(key)
+
+    def iterkeys(self):
+        for i in self._list:
+            yield i
+
+    def iteritems(self):
+        for i in self._list:
+            yield (i, self[i])
+
+def config():
+    c = ConfigParser.ConfigParser()
+    c.read("debian/arch/defines")
+    return c
+
+def config_arch(arch):
+    c = config()
+    c.read("debian/arch/%s/defines" % arch)
+    return c
+
+def config_subarch(arch, subarch):
+    c = config_arch(arch)
+    if subarch is not None:
+        c.read("debian/arch/%s/%s/defines" % (arch, subarch))
+    return c
+
+def list_dirs(dir):
+    ret = []
+    for i in os.listdir(dir):
+        if i not in ('.svn',) and os.path.isdir(os.path.join(dir, i)):
+            ret.append(i)
+    return ret
+
+def list_files(dir):
+    ret = []
+    for i in os.listdir(dir):
+        if os.path.isfile(os.path.join(dir, i)):
+            ret.append(i)
+    return ret
+
+def list_arches():
+    return list_dirs("debian/arch")
+
+def list_subarches(arch):
+    ret = [None]
+    ret.extend(list_dirs("debian/arch/%s" % arch))
+    return ret
+
+def list_flavours(arch, subarch):
+    dir = "debian/arch/%s" % arch
+    if subarch is not None:
+        dir += "/%s" % subarch
+    tmp = list_files(dir)
+    ret = []
+    for i in tmp:
+        if i[:7] == 'config.':
+            ret.append(i[7:])
+    return ret
+
+def read_changelog():
+    r = re.compile(r"""
+^
+(
+    (?P<header>
+        (?P<header_source>\w[-+0-9a-z.]+)\ \((?P<header_version>[^\(\)\ \t]+)\)((\s+[-0-9a-zA-Z]+)+)\;
+    )
+)
+""", re.VERBOSE)
+    f = file("debian/changelog")
+    entries = []
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        line = line.strip('\n')
+        match = r.match(line)
+        if not match:
+            continue
+        if match.group('header'):
+            e = entry()
+            e['Source'] = match.group('header_source')
+            e['Version'] = match.group('header_version')
+            entries.append(e)
+    return entries
+
+def read_rfc822(f):
+    entries = []
+
+    while True:
+        e = entry()
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            line = line.strip('\n')
+            if not line:
+                break
+            if line[0] in ' \t':
+                if not last:
+                    raise ValueError('Continuation line seen before first header')
+                e[last] += '\n' + line
+                continue
+            i = line.find(':')
+            if i < 0:
+                raise ValueError("Not a header, not a continuation: ``%s''" % line)
+            last = line[:i]
+            e[last] = line[i+1:].lstrip()
+        if not e:
+            break
+
+        entries.append(e)
+
+    return entries
+
+def read_template(name):
+    return read_rfc822(file("debian/templates/control.%s.in" % name))
+
+def parse_version(version):
+    match = re.match("^((\d+\.\d+)\..+?)-([^-]+)$", version)
+    return (match.group(0), match.group(1), match.group(2), match.group(3))
+
+def process_depends(key, e, in_e, vars):
+    in_dep = in_e[key].split(',')
+    dep = []
+    for d in in_dep:
+        d = d.strip()
+        d = substitute(d, vars)
+        if d:
+            dep.append(d)
+    if dep:
+        t = ', '.join(dep)
+        e[key] = t
+
+def process_entry(in_entry, vars):
+    e = entry()
+    for i in in_entry.iterkeys():
+        if i in (('Depends', 'Provides', 'Suggests')):
+            process_depends(i, e, in_entry, vars)
+        else:
+            e[i] = substitute(in_entry[i], vars)
+    return e
+
+def process_entries(in_entries, vars):
+    entries = []
+    for i in in_entries:
+        entries.append(process_entry(i, vars))
+    return entries
+
+def process_real_image(in_entry, vars):
+    entry = process_entry(in_entry, vars)
+    for i in (('Depends', 'Provides', 'Suggests')):
+        value = []
+        tmp = entry.get(i, None)
+        if tmp:
+            tmp = tmp.split(',')
+            for t in tmp:
+                value.append(t.strip())
+        if i == 'Depends':
+            t = vars.get('depends', None)
+            if t is not None:
+                value.append(t)
+        elif i == 'Provides':
+            t = vars.get('provides', None)
+            if t is not None:
+                value.append(t)
+        elif i == 'Suggests':
+            t = vars.get('suggests', None)
+            if t is not None:
+                value.append(t)
+        entry[i] = ', '.join(value)
+    if vars.has_key('desc'):
+        entry['Description'] += '\n .\n ' + '\n '.join(textwrap.wrap(vars['desc']))
+    return entry
+
+def process_real_tree(in_entry, changelog, vars):
+    entry = process_entry(in_entry, vars)
+    tmp = changelog[0]['Source']
+    versions = []
+    for i in changelog:
+        if i['Source'] != tmp:
+            break
+        versions.insert(0, i['Version'])
+    for i in (('Depends', 'Provides')):
+        value = []
+        tmp = entry.get(i, None)
+        if tmp:
+            tmp = tmp.split(',')
+            for t in tmp:
+                value.append(t.strip())
+        if i == 'Depends':
+            tmp = []
+            for v in versions:
+                v = parse_version(v)
+                tmp.append("linux-source-%s (= %s)" % (v[1], v[0]))
+            value.append(' | '.join(tmp))
+        elif i == 'Provides':
+            for v in versions:
+                v = parse_version(v)
+                value.append("linux-tree-%s" % v[0])
+        entry[i] = ', '.join(value)
+    return entry
+
+def substitute(s, vars):
+    def subst(match):
+        return vars[match.group(1)]
+    return re.sub(r'@([^@]+)@', subst, s)
+
+def vars_changelog(vars, changelog):
+    version = parse_version(changelog[0]['Version'])
+    vars['srcver'] = version[0]
+    vars['version'] = version[1]
+    vars['major'] = version[2]
+    return vars
+
+def write_control(list):
+    write_rfc822(file("debian/control", 'w'), list)
+
+def write_rfc822(f, list):
+    for i in list:
+        for j in i.iteritems():
+            f.write("%s: %s\n" % j)
+        f.write('\n')
+
+if __name__ == '__main__':
+    changelog = read_changelog()
+
+    vars = {}
+    vars = vars_changelog(vars, changelog)
+    vars.update(config().defaults())
+
+    arches = {}
+    subarches_architecture = {}
+    for arch in list_arches():
+        t1 = {}
+        for subarch in list_subarches(arch):
+            t2 = {}
+            for flavour in list_flavours(arch, subarch):
+                t2[flavour] = True
+            t1[subarch] = t2
+            t3 = subarches_architecture.get(subarch, {})
+            t3[arch] = True
+            subarches_architecture[subarch] = t3
+        arches[arch] = t1
+
+    packages = []
+
+    source = read_template("source")
+    packages.append(process_entry(source[0], vars))
+
+    main = read_template("main")
+    packages.extend(process_entries(main, vars))
+
+    tree = read_template("tree")
+    packages.append(process_real_tree(tree[0], changelog, vars))
+
+    headers = read_template("headers")
+    a = subarches_architecture[None].keys()
+    a.sort()
+    b = vars.copy()
+    b['arch'] = ' '.join(a)
+    packages.extend(process_entries(headers, b))
+
+    headers_flavour = read_template("headers.flavour")
+    image = read_template("image")
+
+    i1 = arches.keys()
+    i1.sort()
+    for arch in i1:
+        arch_vars = vars.copy()
+        arch_vars['arch'] = arch
+        arch_vars.update(config_arch(arch).defaults())
+        i2 = arches[arch].keys()
+        i2.sort()
+        for subarch in i2:
+            subarch_config = config_subarch(arch, subarch)
+            subarch_vars = arch_vars.copy()
+            subarch_vars.update(subarch_config.defaults())
+            if subarch is not None:
+                subarch_vars['subarch'] = '%s-' % subarch
+            else:
+                subarch_vars['subarch'] = ''
+            i3 = arches[arch][subarch].keys()
+            i3.sort()
+            for flavour in i3:
+                flavour_vars = subarch_vars.copy()
+                flavour_vars['flavour'] = flavour
+                try:
+                    flavour_vars.update(dict(subarch_config.items(flavour)))
+                except ConfigParser.NoSectionError: pass
+                if not flavour_vars.has_key('class'):
+                    flavour_vars['class'] = '%s-class' % flavour
+                if not flavour_vars.has_key('longclass'):
+                    flavour_vars['longclass'] = flavour_vars['class']
+
+                packages.extend(process_entries(headers_flavour, flavour_vars))
+                packages.append(process_real_image(image[0], flavour_vars))
+
+    write_control(packages)
+
