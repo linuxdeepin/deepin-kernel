c3687a0f8ceaea58ded4ec439f46006c566de32b
Bastian Blank <waldi@debian.org>
2006-07-19 22:56:40 +0000

    Replace own modpost with a dispatcher and four version of the original one.
    * src/mod/Makefile: Build all modpost variants.
    * src/mod/Makefile.real, src/mod/elfconfig.h, src/mod/modpost.c,
      src/mod/real-lsb-32/elfconfig.h, src/mod/real-lsb-64/elfconfig.h,
      src/mod/real-msb-32/elfconfig.h, src/mod/real-msb-64/elfconfig.h: Add.
    * src/mod/real-lsb-32, src/mod/real-lsb-64, src/mod/real-msb-32,
      src/mod/real-msb-64: New directory.
    * src/mod/elf.cpp, src/mod/elf.hpp, src/mod/endian.hpp, src/mod/modpost.cpp,
      src/mod/module.cpp, src/mod/module.hpp, src/mod/module_devicetable.cpp,
      src/mod/module_devicetable.hpp, src/mod/module_devicetable.tpp,
      src/mod/module_devicetable_impl_2_6_16.cpp,
      src/mod/module_devicetable_impl_2_6_16.hpp: Remove.
    
    svn path=/dists/trunk/linux-kbuild-2.6/; revision=7045

---
+++ src/mod/modpost.c
@@ -0,0 +1,69 @@
+#include <elf.h>
+#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main (int argc, char *argv[])
+{
+  char const *data, *class;
+  char prog[1024];
+  unsigned char ei[EI_NIDENT];
+  int opt;
+  FILE *file;
+
+  while ((opt = getopt (argc, argv, "ai:I:mo:")) != -1)
+  { 
+    switch(opt)
+    {
+      case 'a':
+      case 'i':
+      case 'I':
+      case 'm':
+      case 'o':
+        break;
+      default:
+        return 1;
+    }
+  }
+
+  if (!(file = fopen (argv[optind], "r")))
+  {
+    fprintf (stderr, "Can't open file\n");
+    return 1;
+  }
+  if (fread (ei, 1, EI_NIDENT, file) != EI_NIDENT)
+  {
+    fprintf (stderr, "Error: input truncated\n");
+    return 1;
+  }
+  if (memcmp (ei, ELFMAG, SELFMAG) != 0)
+  {
+    fprintf (stderr, "Error: not ELF\n");
+    return 1;
+  }
+  switch (ei[EI_DATA]) {
+    case ELFDATA2LSB:
+      data = "lsb";
+      break;
+    case ELFDATA2MSB:
+      data = "msb";
+      break;
+    default:
+      return 1;
+  }
+  switch (ei[EI_CLASS]) {
+    case ELFCLASS32:
+      class = "32";
+      break;
+    case ELFCLASS64:
+      class = "64";
+      break;
+    default:
+      return 1;
+  }
+  snprintf (prog, sizeof prog, "%s.real-%s-%s", argv[0], data, class);
+
+  return execv (prog, argv);
+}
