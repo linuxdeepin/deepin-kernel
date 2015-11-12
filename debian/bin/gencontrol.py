91416e456c4580a8b70b6a0266e992fcb15b0300
Ben Hutchings <ben@decadent.org.uk>
2015-11-12 15:12:02 +0000

    gencontrol.py: Delete redundant arguments to ConfigCore.merge

--- debian/bin/gencontrol.py
+++ debian/bin/gencontrol.py
@@ -109,7 +109,7 @@ class Gencontrol(Base):
         makeflags = makeflags.copy()
         makeflags['ALL_FEATURESETS'] = ' '.join(fs_enabled)
         makeflags['ALL_TRIPLETS'] = ' '.join(triplet_enabled)
-        if not self.config.merge('docs', None, None).get('enabled', True):
+        if not self.config.merge('docs').get('enabled', True):
             makeflags['DO_DOCS'] = False
         super(Gencontrol, self).do_main_makefile(makefile, makeflags, extra)
 
@@ -118,7 +118,7 @@ class Gencontrol(Base):
 
     def do_main_packages(self, packages, vars, makeflags, extra):
         packages.extend(self.process_packages(self.templates["control.main"], self.vars))
-        if self.config.merge('docs', None, None).get('enabled', True):
+        if self.config.merge('docs').get('enabled', True):
             packages.extend(self.process_packages(self.templates["control.docs"], self.vars))
 
     arch_makeflags = (
