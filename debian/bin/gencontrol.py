3df161c94fd5adc651c5ae34d643a59fc2530070
Ben Hutchings <ben@decadent.org.uk>
2016-06-06 16:10:52 +0100

    linux-image: Use linux-check-removal command in prerm; remove debconf templates
    This removes most of the remaining complexity from the script.

--- debian/bin/gencontrol.py
+++ debian/bin/gencontrol.py
@@ -77,18 +77,6 @@ class Gencontrol(Base):
             'SOURCEVERSION': self.version.complete,
         })
 
-        # Prepare to generate template-substituted translations
-        try:
-            os.mkdir('debian/po')
-        except OSError:
-            pass
-        for path in glob.glob('debian/templates/po/*.po'):
-            target = 'debian/po/' + os.path.basename(path)
-            with open(target, 'w') as f:
-                f.write('# THIS IS A GENERATED FILE; DO NOT EDIT IT!\n'
-                        '# Translators should edit %s instead.\n'
-                        '#\n' % path)
-
         # Prepare to generate debian/tests/control
         self.tests_control = None
 
@@ -473,14 +461,10 @@ class Gencontrol(Base):
         self._substitute_file('headers.postinst', vars,
                               'debian/linux-headers-%s%s.postinst' %
                               (vars['abiname'], vars['localversion']))
-        for name in ['postinst', 'postrm', 'preinst', 'prerm', 'templates']:
+        for name in ['postinst', 'postrm', 'preinst', 'prerm']:
             self._substitute_file('image.%s' % name, vars,
                                   'debian/linux-image-%s%s.%s' %
                                   (vars['abiname'], vars['localversion'], name))
-        for path in glob.glob('debian/templates/po/*.po'):
-            self._substitute_file('po/' + os.path.basename(path), vars,
-                                  'debian/po/' + os.path.basename(path),
-                                  append=True)
         if build_debug:
             self._substitute_file('image-dbg.lintian-override', vars,
                                   'debian/linux-image-%s%s-dbg.lintian-overrides' %
