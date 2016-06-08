72f58e83eaacb07b7dc5d077890f2be7d548300b
Ben Hutchings <ben@decadent.org.uk>
2016-06-08 23:31:55 +0100

    linux-image: Add '-unsigned' suffix to packages with modules that will later be signed

--- debian/bin/gencontrol.py
+++ debian/bin/gencontrol.py
@@ -355,7 +355,10 @@ class Gencontrol(Base):
         packages_dummy = []
         packages_own = []
 
-        image = self.templates["control.image"]
+        build_signed = config_entry_build.get('signed-modules')
+
+        image = self.templates[build_signed and "control.image-unsigned"
+                               or "control.image"]
 
         config_entry_xen = self.config.merge('xen', arch, featureset, flavour)
         if config_entry_xen:
@@ -370,6 +373,7 @@ class Gencontrol(Base):
 
         image_main = self.process_real_image(image[0], image_fields, vars)
         packages_own.append(image_main)
+        makeflags['IMAGE_PACKAGE_NAME'] = image_main['Package']
         packages_own.extend(self.process_packages(image[1:], vars))
 
         package_headers = self.process_package(headers[0], vars)
@@ -452,7 +456,7 @@ class Gencontrol(Base):
         makeflags['KCONFIG_OPTIONS'] = ''
         if build_debug:
             makeflags['KCONFIG_OPTIONS'] += ' -o DEBUG_INFO=y'
-        if config_entry_build.get('signed-modules'):
+        if build_signed:
             makeflags['KCONFIG_OPTIONS'] += ' -o MODULE_SIG=y'
 
         cmds_binary_arch = ["$(MAKE) -f debian/rules.real binary-arch-flavour %s" % makeflags]
@@ -473,8 +477,7 @@ class Gencontrol(Base):
                               (vars['abiname'], vars['localversion']))
         for name in ['postinst', 'postrm', 'preinst', 'prerm']:
             self._substitute_file('image.%s' % name, vars,
-                                  'debian/linux-image-%s%s.%s' %
-                                  (vars['abiname'], vars['localversion'], name))
+                                  'debian/%s.%s' % (image_main['Package'], name))
         if build_debug:
             self._substitute_file('image-dbg.lintian-override', vars,
                                   'debian/linux-image-%s%s-dbg.lintian-overrides' %
