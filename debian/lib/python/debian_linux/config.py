9747655f89997099ffc5a73f065d22201b1e24c1
Ben Hutchings <ben@decadent.org.uk>
2017-01-07 18:25:56 +0000

    Make the pickled config (config.defines.dump) reproducible

--- debian/lib/python/debian_linux/config.py
+++ debian/lib/python/debian_linux/config.py
@@ -1,3 +1,4 @@
+import collections
 import os
 import os.path
 import pickle
@@ -43,7 +44,8 @@ class SchemaItemList(object):
         return [j.strip() for j in re.split(self.type, i)]
 
 
-class ConfigCore(dict):
+# Using OrderedDict instead of dict makes the pickled config reproducible
+class ConfigCore(collections.OrderedDict):
     def get_merge(self, section, arch, featureset, flavour, key, default=None):
         temp = []
 
